d9dbcd2ffce462e99f8405525779b6a70596f25e
MaMelindaG <gitomamelinda@gmail.com>
2021-05-01 01:15:56 +0800

    Printing Elements from storage on Load

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,12 @@ eventListeners();
 function eventListeners() {
      //  Form Submission 
      document.querySelector('#form').addEventListener('submit', newTweet);
+
+      // Remove tweet from the list 
+      tweetList.addEventListener('click', removeTweet);
+       
+      // Document 
+      document.addEventListener ('DOMContentLoaded', localStorageOnLoad);
 }
 
 
@@ -33,7 +39,7 @@ function newTweet(e) {
      // Add the remove button to each tweet
      li.appendChild(removeBtn);
      
-     // Add to the list
+     // Add to the lists
      tweetList.appendChild(li);
 
      // add to Local Storage
@@ -70,4 +76,27 @@ function getTweetsFromStorage() {
           tweets = JSON.parse( tweetsLS );
      }
      return tweets;
+}
+
+// Prints Local Storage Tweets  on load 
+function localStorageOnLoad() {
+    let tweets = getTweetsFromStorage();
+    
+    // loop thought storage and then print the values
+    tweets.forEach(function(tweet) {
+         // Create the remove button
+         const removeBtn = document.createElement('a');
+         removeBtn.classList = 'remove-tweet';
+         removeBtn.textContent = 'X';
+    
+         // Create an <li> element
+         const li = document.createElement('li');
+         li.textContent = tweet;
+    
+         // Add the remove button to each tweet
+         li.appendChild(removeBtn);
+    
+         // Add to the list
+         tweetList.appendChild(li);
+    });
 }
